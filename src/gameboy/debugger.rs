abdd8adcea14f0e469722ef5b772345f27a9991a
Antoine BAGNAUD <bagnaud.antoine@gmail.com>
2017-04-11 19:42:33 +0200

    WebSocket debug handler

--- src/gameboy/debugger.rs
+++ src/gameboy/debugger.rs
@@ -1,21 +1,30 @@
-use std::io;
-use std::io::prelude::*;
+extern crate ws;
+use self::ws::{CloseCode, Handler, Message, Result, Sender, listen};
+use std::thread;
 
 pub struct Debugger {
     pub step: u64,
 }
 
+struct Websocket(Sender);
+
 impl Debugger {
-    pub fn new() -> Option<Debugger> {
-        Some(Debugger { step: 0 })
+    pub fn new() -> Debugger {
+        Debugger { step: 0 }
     }
 
-    pub fn prompt(&mut self) -> String {
-        print!("debug> ");
-        let _ = io::stdout().flush();
-        let mut cmd = String::new();
-        io::stdin().read_line(&mut cmd).unwrap();
-        self.step += 1;
-        cmd.trim().into()
+    fn listen(&self) {
+        thread::spawn(move || { listen("127.0.0.1:3012", |out| Websocket(out)).unwrap(); });
+    }
+}
+
+
+impl Handler for Websocket {
+    fn on_message(&mut self, _: Message) -> Result<()> {
+        unimplemented!();
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("WebSocket closing for ({:?}) {}", code, reason);
     }
 }
